af85051a8b5adb83e0b8da1e3d492e12aa314abd
DuskVixen <phe21001@byui.edu>
2022-01-21 19:24:48 -0700

    hi

--- js/total.js
+++ js/total.js
@@ -4,7 +4,18 @@ function getTotal() {
 	// INPUT
 	 let q1 = 0;
 	 let p1 = 0; 
-	
+     let q2 = 0;
+     let p2 = 0;
+     let q3 = 0;
+     let p3 = 0;
+
+     q1 = parseFloat(document.querySelector('#q1').value);
+     p1 = parseFloat(document.querySelector('#p1').value);
+     q2 = parseFloat(document.querySelector('#q2').value);
+     p2 = parseFloat(document.querySelector('#p2').value);
+     q3 = parseFloat(document.querySelector('#q3').value);
+     p3 = parseFloat(document.querySelector('#p3').value);
+    	
 	// PROCESSING
 	 
 	
